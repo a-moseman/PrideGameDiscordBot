841357a4992a9cf6bbce509badf8da9e7d0fa59f
a-moseman <drewmoseman@gmail.com>
2022-10-11 09:43:37 -0400

    Refactor loading code.

--- ClickerGame/src/main/java/Game/PersistenceManager.java
+++ ClickerGame/src/main/java/Game/PersistenceManager.java
@@ -2,10 +2,8 @@ package Game;
 
 import SpellBook.Spell;
 import SpellBook.SpellBookModule;
-import SpellBook.SpellEffect;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.File;
@@ -33,66 +31,66 @@ public class PersistenceManager {
                 e.printStackTrace();
                 return;
             }
-            JsonNode playersNode = root.get("players");
-            Player player;
-            JsonNode playerNode;
-            String uuid;
-            long lastCollectionTime;
-            JsonNode statsNode;
-            long pride;
-            long shame;
-            long ego;
-            long guilt;
-            long honor;
-            long dishonor;
-            JsonNode spellBookNode;
-            JsonNode spellsNode;
-            JsonNode spellNode;
-            String spellName;
-            int spellLevel;
-            ArrayList<Spell> spells = new ArrayList<>();
-            JsonNode currentSpellEffectNode;
-            double collectionCritChance;
-            boolean prideFavored;
-            boolean shameFavored;
-            long bountifulness;
-            PlayerStats stats;
-            SpellBookModule spellBookModule;
-            for (int i = 0; i < playersNode.size(); i++) {
-                playerNode = playersNode.get(i);
-                uuid = playerNode.get("uuid").asText();
-                lastCollectionTime = playerNode.get("last_collection_time").asLong();
-                statsNode = playerNode.get("stats");
-                pride = statsNode.get("pride").asLong();
-                shame = statsNode.get("shame").asLong();
-                ego = statsNode.get("ego").asLong();
-                guilt = statsNode.get("guilt").asLong();
-                honor = statsNode.get("honor").asLong();
-                dishonor = statsNode.get("dishonor").asLong();
-                stats = new PlayerStats(pride, ego, honor, shame, guilt, dishonor);
-                spellBookNode = playerNode.get("spell_book");
-                if (spellBookNode != null) {
-                    spellsNode = spellBookNode.get("spells");
-                    for (int j = 0; j < spellsNode.size(); j++) {
-                        spellNode = spellsNode.get(j);
-                        spellName = spellNode.get("name").asText();
-                        spellLevel = spellNode.get("level").asInt();
-                        spells.add(SpellBookModule.generateSpell(spellName, spellLevel));
-                    }
-                    currentSpellEffectNode = spellBookNode.get("current_spell_effect");
-                    collectionCritChance = currentSpellEffectNode.get("collection_crit_chance").asDouble();
-                    prideFavored = currentSpellEffectNode.get("pride_favored").asBoolean();
-                    shameFavored = currentSpellEffectNode.get("shame_favored").asBoolean();
-                    bountifulness = currentSpellEffectNode.get("bountifulness").asLong();
-                    spellBookModule = new SpellBookModule(spells, collectionCritChance, prideFavored, shameFavored, bountifulness);
-                    player = new Player(uuid, stats, lastCollectionTime, spellBookModule);
-                }
-                else {
-                    player = new Player(uuid, stats, lastCollectionTime);
-                }
-                gameModel.addPlayer(uuid, player);
-            }
+            loadPlayers(root);
         }
+    }
+
+    private void loadPlayers(JsonNode root) {
+        JsonNode playersNode = root.get("players");
+        Player player;
+        JsonNode playerNode;
+        String uuid;
+        long lastCollectionTime;
+        PlayerStats stats;
+        for (int i = 0; i < playersNode.size(); i++) {
+            playerNode = playersNode.get(i);
+            uuid = playerNode.get("uuid").asText();
+            lastCollectionTime = playerNode.get("last_collection_time").asLong();
+            stats = loadPlayerStats(playerNode);
+            SpellBookModule spellBookModule = loadSpellBookModule(playerNode);
+            if (spellBookModule != null) {
+                player = new Player(uuid, stats, lastCollectionTime, spellBookModule);
+            }
+            else {
+                player = new Player(uuid, stats, lastCollectionTime);
+            }
+            gameModel.addPlayer(uuid, player);
+        }
+    }
+
+    private PlayerStats loadPlayerStats(JsonNode playerNode) {
+        JsonNode statsNode = playerNode.get("stats");
+        long pride = statsNode.get("pride").asLong();
+        long shame = statsNode.get("shame").asLong();
+        long ego = statsNode.get("ego").asLong();
+        long guilt = statsNode.get("guilt").asLong();
+        long honor = statsNode.get("honor").asLong();
+        long dishonor = statsNode.get("dishonor").asLong();
+        return new PlayerStats(pride, ego, honor, shame, guilt, dishonor);
+    }
+
+    private SpellBookModule loadSpellBookModule(JsonNode playerNode) {
+        JsonNode spellBookNode = playerNode.get("spell_book");
+        if (spellBookNode == null) {
+            return null;
+        }
+        JsonNode spellsNode = spellBookNode.get("spells");
+        ArrayList<Spell> spells = new ArrayList<>();
+        JsonNode spellNode;
+        String spellName;
+        int spellLevel;
+        for (int j = 0; j < spellsNode.size(); j++) {
+            spellNode = spellsNode.get(j);
+            spellName = spellNode.get("name").asText();
+            spellLevel = spellNode.get("level").asInt();
+            spells.add(SpellBookModule.generateSpell(spellName, spellLevel));
+        }
+        JsonNode currentSpellEffectNode = spellBookNode.get("current_spell_effect");
+        double collectionCritChance = currentSpellEffectNode.get("collection_crit_chance").asDouble();
+        boolean prideFavored = currentSpellEffectNode.get("pride_favored").asBoolean();
+        boolean  shameFavored = currentSpellEffectNode.get("shame_favored").asBoolean();
+        long bountifulness = currentSpellEffectNode.get("bountifulness").asLong();
+        return new SpellBookModule(spells, collectionCritChance, prideFavored, shameFavored, bountifulness);
     }
 
     protected void save() {
