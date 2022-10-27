395252dc1110766bc20ac4b56ea99b7d33ede0f3
a-moseman <drewmoseman@gmail.com>
2022-10-26 21:31:47 -0400

    Add things describe can describe.

--- src/main/java/PrideBot/Bot/BotModel.java
+++ src/main/java/PrideBot/Bot/BotModel.java
@@ -9,12 +9,12 @@ import net.dv8tion.jda.api.entities.Guild;
 import java.util.Locale;
 
 public class BotModel {
-    private static final Response ERR_INVALID_COMMAND = new Response("ERROR",  "Invalid command.");
-    private static final Response ERR_TOO_MANY_ARGS = new Response("ERROR",  "Too many arguments.");
-    private static final Response ERR_MISSING_ARGS = new Response("ERROR", "Missing arguments.");
-    private static final Response ERR_INVALID_ARG = new Response("ERROR",  "Invalid argument.");
-    private static final Response ERR_PLAYER_DNE = new Response("ERROR",  "Player does not exist.");
-    private static final Response ERR_MISSING_SPELL_BOOK_MODULE = new Response("ERROR", "Missing spell book module.");
+    private static final Response ERR_INVALID_COMMAND = new Response("Invalid command.");
+    private static final Response ERR_TOO_MANY_ARGS = new Response("Too many arguments.");
+    private static final Response ERR_MISSING_ARGS = new Response("Missing arguments.");
+    private static final Response ERR_INVALID_ARG = new Response("Invalid argument.");
+    private static final Response ERR_PLAYER_DNE = new Response("Player does not exist.");
+    private static final Response ERR_MISSING_SPELL_BOOK_MODULE = new Response("Missing spell book module.");
     private static final String COMMANDS_MESSAGE_BLESS = "p>bless <target> <amount> - Grants the target player the given amount of pride. The target argument must be a mention and the amount argument must be a positive integer. (pride_dm only)";
     private static final String COMMANDS_MESSAGE_CURSE = "p>curse <target> <amount> - Grants the target player the given amount of shame. The target argument must be a mention and the amount argument must be a positive integer. (pride_dm only)";
     private static final String COMMANDS_MESSAGE_COLLECT = "p>collect - Collect your daily pride or shame. If you currently have more pride, you will collect pride, and vice versa.";
@@ -60,12 +60,12 @@ public class BotModel {
         switch (command.getTerm(0).toUpperCase(Locale.ROOT)) {
             case "BLESS":
                 if (!isAdmin) {
-                    return new Response("ERROR", command.getAuthor().getName() + ", you do not have the pride_dm role.");
+                    return new Response(command.getAuthor().getName() + ", you do not have the pride_dm role.");
                 }
                 return bless(command, guild);
             case "CURSE":
                 if (!isAdmin) {
-                    return new Response("ERROR", command.getAuthor().getName() + ", you do not have the pride_dm role.");
+                    return new Response(command.getAuthor().getName() + ", you do not have the pride_dm role.");
                 }
                 return curse(command, guild);
             case "COLLECT":
@@ -93,27 +93,127 @@ public class BotModel {
         }
     }
 
+    private static final Response PRIDE_DESCRIPTION = new Response("Pride:" +
+            "\n\tOne of two atomic values in the game. Used as a currency."
+    );
+    private static final Response SHAME_DESCRIPTION = new Response("Shame:" +
+            "\n\tOne of two atomic values of the game. Used as a currency."
+    );
+    private static final Response AFFINITY_DESCRIPTION = new Response("Affinity:" +
+            "\n\tYou can either have affinity for Pride or Shame." +
+            "\n\tAffinity is determined by comparing in order: honor and dishonor, ego and guilt, pride and shame." +
+            "\n\tYour affinity determines what you when you collect."
+    );
+    private static final Response COLLECTION_DESCRIPTION = new Response("Collection:" +
+            "\n\tThe action of collecting your daily pride or shame, based on your affinity." +
+            "\n\tThe amount you collect is 1 + x + b, where x is your ego or guilt and b is your bountifulness." +
+            "\n\tEgo is used for x if you're collecting pride, guilt is used for x is you're collecting shame." +
+            "\n\tOn a crit, the amount you collect is 2 * (1 + x + b)"
+    );
+    private static final Response EGO_DESCRIPTION = new Response("Ego:" +
+            "\n\tEgo can be bought with pride. It costs (e + 1) * 7 pride, where e is your current ego." +
+            "\n\tThe more ego you have, the more pride you gain when you collect." +
+            "\n\tEgo is also used to pay for honor."
+    );
+    private static final Response GUILT_DESCRIPTION = new Response("Ego:" +
+            "\n\tGuilt can be bought with shame. It costs (g + 1) * 7 shame, where g is your current guilt." +
+            "\n\tThe more guilt you have, the more guilt you gain when you collect." +
+            "\n\tGuilt is also used to pay for dishonor."
+    );
+    private static final Response HONOR_DESCRIPTION = new Response("Honor: " +
+            "\n\tHonor is used to determine your level, which grants access to features in the game." +
+            "\n\tIt costs ego to buy. Specifically, you need (h + 1) * 10 ego to buy 1 honor, where h is your current honor." +
+            "\n\tHowever, do note that you spend all of your ego when you buy honor, the cost is just the minimum requirement."
+    );
+    private static final Response DISHONOR_DESCRIPTION = new Response("Honor: " +
+            "\n\tDishonor is used to determine your level, which grants access to features in the game." +
+            "\n\tIt costs guilt to buy. Specifically, you need (d + 1) * 10 guilt to buy 1 dishonor, where d is your current dishonor." +
+            "\n\tHowever, do note that you spend all of your guilt when you buy dishonor, the cost is just the minimum requirement."
+    );
+    private static final Response LEVEL_DESCRIPTION = new Response("Level: " +
+            "\n\tYour level determines your access to game features." +
+            "\n\tIt is equal to your honor or dishonor, whatever is greater." +
+            "\n\tLevel 0 grants access to the base game." +
+            "\n\tLevel 1 grants access to your spell book and the spell system." +
+            "\n\tThere are no more levels beyond this, although you can attain them." +
+            "\n\tYou will gain access to features retroactively when they are implemented, if you already have the needed level."
+    );
+    private static final Response SPELLBOOK_DESCRIPTION = new Response("Spellbook:" +
+            "\n\tAt level 1, you gain access to your spellbook." +
+            "\n\tYour spellbook grants you the ability to store spells, cast spells, and try to buy spells." +
+            "\n\tSpells grant various effects that benefit the amount of pride or shame you gain when you next collect." +
+            "\n\tThe effects are:" +
+            "\n\t\tBountifulness - A flat bonus added to the amount of pride or shame you collect." +
+            "\n\t\tCrit Chance - A probability to double your pride or shame collected." +
+            "\n\t\tPride Favored - A guarantee that you will collect pride." +
+            "\n\t\tShame Favored - A guarantee that you will collect shame."
+    );
+    private static final Response SPELLS_DESCRIPTION = new Response("Spells:" +
+            "\n\tSpells can be bought using the buy command." +
+            "\n\tHowever, what you get is random, assuming you even get one." +
+            "\n\tOn the bright side, they are cheap, only costing one pride or shame--what ever you have most of." +
+            "\n\tYou can cast these spells using the cast command and providing the index of the spell in your spellbook (use p>spells to get a list of your spells and their indices)." +
+            "\n\tWhen cast, they will grant buffs to your next collection." +
+            "\n\tSpells have a number of uses and you will lose the spell once those uses reach zero." +
+            "\n\tEach spell can also only be cast once per day." +
+            "\n\tHowever, the spell effects do stack, with the exception of pride favored and shame favored which override." +
+            "\n\tTo get information on a spell you have, use p>describe spell <index>, where index is the index of the spell in your spellbook."
+    );
+
+
     private Response describe(Command command) {
         switch (command.getTerm(1).toUpperCase(Locale.ROOT)) {
-            case "SPELL":
-                int spellIndex;
-                try {
-                    spellIndex = Integer.parseInt(command.getTerm(2));
-                }
-                catch (Exception e) {
-                    return ERR_INVALID_ARG;
-                }
-                String spellDescription;
-                try {
-                    spellDescription = api.getSpellDescription(command.getAuthor().getId(), spellIndex);
-                }
-                catch (Exception e) {
-                    return ERR_INVALID_ARG;
-                }
-                return new Response("MESSAGE", spellDescription);
+            case "SPELL": // describe a given spell in the player's spell book based on index
+                return describeSpell(command);
+            case "PRIDE":
+                return PRIDE_DESCRIPTION;
+            case "SHAME":
+                return SHAME_DESCRIPTION;
+            case "AFFINITY":
+                return AFFINITY_DESCRIPTION;
+            case "COLLECT":
+            case "COLLECTION":
+            case "COLLECTING":
+                return COLLECTION_DESCRIPTION;
+            case "EGO":
+                return EGO_DESCRIPTION;
+            case "GUILT":
+                return GUILT_DESCRIPTION;
+            case "HONOR":
+                return HONOR_DESCRIPTION;
+            case "DISHONOR":
+                return DISHONOR_DESCRIPTION;
+            case "LEVEL":
+            case "LEVELS":
+                return LEVEL_DESCRIPTION;
+            case "SPELLBOOK":
+                return SPELLBOOK_DESCRIPTION;
+            case "SPELLS":
+                return SPELLS_DESCRIPTION;
             default:
-                return ERR_INVALID_ARG;
+                return new Response("I do not have a description for that.");
         }
+    }
+
+    private Response describeSpell(Command command) {
+        if (command.getSize() < 3) { // the player is NOT trying to get info on a specific spell they possess.
+            return SPELLS_DESCRIPTION;
+        }
+        int spellIndex;
+        try {
+            spellIndex = Integer.parseInt(command.getTerm(2));
+        }
+        catch (Exception e) {
+            return ERR_INVALID_ARG;
+        }
+        String spellDescription;
+        try {
+            spellDescription = api.getSpellDescription(command.getAuthor().getId(), spellIndex);
+        }
+        catch (Exception e) {
+            return ERR_INVALID_ARG;
+        }
+        return new Response(spellDescription);
     }
 
     private Response cast(Command command) {
@@ -135,9 +235,9 @@ public class BotModel {
         }
         String spellName = api.getSpellName(command.getAuthor().getId(), spellIndex) + " lv. " + api.getSpellLevel(command.getAuthor().getId(), spellIndex);
         if (api.castSpell(command.getAuthor().getId(), spellIndex)) {
-            return new Response("MESSAGE", spellName + " cast successfully.");
+            return new Response(spellName + " cast successfully.");
         }
-        return new Response("MESSAGE", "Failed to cast " + spellName + ".");
+        return new Response("Failed to cast " + spellName + ".");
     }
 
     private Response spells(Command command) {
@@ -148,8 +248,7 @@ public class BotModel {
             return ERR_MISSING_SPELL_BOOK_MODULE;
         }
         return new Response(
-                "MESSAGE",
-                command.getAuthor().getName() + "'s Spells: " +
+                command.getAuthor().getName() + "'s Spells: \n" +
                 api.getSpellList(command.getAuthor().getId()));
     }
 
@@ -173,10 +272,10 @@ public class BotModel {
             return ERR_INVALID_ARG;
         }
         if (pride < 0) {
-            return new Response("MESSAGE", "Amount must be a positive integer.");
+            return new Response("Amount must be a positive integer.");
         }
         api.bless(targetUUID, pride);
-        return new Response("MESSAGE", guild.getMemberById(targetUUID).getEffectiveName() + ", you have been blessed for " + pride + " pride.");
+        return new Response(guild.getMemberById(targetUUID).getEffectiveName() + ", you have been blessed for " + pride + " pride.");
     }
 
     private Response curse(Command command, Guild guild) {
@@ -199,10 +298,10 @@ public class BotModel {
             return ERR_INVALID_ARG;
         }
         if (shame < 0) {
-            return new Response("MESSAGE", "Amount must be a positive integer.");
+            return new Response("Amount must be a positive integer.");
         }
         api.curse(targetUUID, shame);
-        return new Response("MESSAGE", guild.getMemberById(targetUUID).getEffectiveName() + ", you have been cursed for " + shame + " shame.");
+        return new Response(guild.getMemberById(targetUUID).getEffectiveName() + ", you have been cursed for " + shame + " shame.");
     }
 
     private Response collect(Command command) {
@@ -210,14 +309,14 @@ public class BotModel {
             return ERR_TOO_MANY_ARGS;
         }
         if (api.collect(command.getAuthor().getId())) {
-            return new Response("MESSAGE", command.getAuthor().getName() + ", you have completed your daily collection.");
+            return new Response(command.getAuthor().getName() + ", you have completed your daily collection.");
         }
         else {
             double t = ((double) ((int) (api.getDaysUntilNextCollection(command.getAuthor().getId()) * 1000))) / 1000;
             t += 0.005;
             t = ((double)(int)(t * 100)) / 100;
 
-            return new Response("MESSAGE", command.getAuthor().getName() + ", you cannot collect at the moment. Try again in " + t + " days.");
+            return new Response(command.getAuthor().getName() + ", you cannot collect at the moment. Try again in " + t + " days.");
         }
     }
 
@@ -277,9 +376,9 @@ public class BotModel {
         }
         if (i == 0) {
             BuyFailResult buyFailResult = (BuyFailResult) buyResult;
-            return new Response("MESSAGE", username + ", you cannot afford to buy any ego.\nYou need " + buyFailResult.MISSING_CURRENCY + " more pride to buy 1 ego.");
+            return new Response(username + ", you cannot afford to buy any ego.\nYou need " + buyFailResult.MISSING_CURRENCY + " more pride to buy 1 ego.");
         }
-        return new Response("MESSAGE", username + ", you have bought " + i + " ego for " + spentPride + " pride.");
+        return new Response(username + ", you have bought " + i + " ego for " + spentPride + " pride.");
     }
 
     private Response buyGuilt(String uuid, String username, int amount) {
@@ -292,36 +391,36 @@ public class BotModel {
         }
         if (i == 0) {
             BuyFailResult buyFailResult = (BuyFailResult) buyResult;
-            return new Response("MESSAGE", username + ", you can not afford to buy guilt.\n You need " + buyFailResult.MISSING_CURRENCY + " more shame to buy 1 guilt.");
+            return new Response(username + ", you can not afford to buy guilt.\n You need " + buyFailResult.MISSING_CURRENCY + " more shame to buy 1 guilt.");
         }
-        return new Response("MESSAGE", username + ", you have bought " + i + " guilt for " + spentShame + " shame.");
+        return new Response(username + ", you have bought " + i + " guilt for " + spentShame + " shame.");
     }
 
     private Response buyHonor(String uuid, String username) {
         BuyResult buyResult = api.buyHonor(uuid);
         if (buyResult instanceof BuySuccessResult) {
-            return new Response("MESSAGE", username + ", you bought 1 honor for all of your ego.");
+            return new Response(username + ", you bought 1 honor for all of your ego.");
         }
         BuyFailResult buyFailResult = (BuyFailResult) buyResult;
-        return new Response("MESSAGE", username + ", you can not afford to buy honor.\nYou need " + buyFailResult.MISSING_CURRENCY + " more ego to buy 1 honor.");
+        return new Response(username + ", you can not afford to buy honor.\nYou need " + buyFailResult.MISSING_CURRENCY + " more ego to buy 1 honor.");
     }
 
     private Response buyDishonor(String uuid, String username) {
         BuyResult buyResult = api.buyDishonor(uuid);
         if (buyResult instanceof BuySuccessResult) {
-            return new Response("MESSAGE", username + ", you bought 1 dishonor for all of your guilt.");
+            return new Response(username + ", you bought 1 dishonor for all of your guilt.");
         }
         BuyFailResult buyFailResult = (BuyFailResult) buyResult;
-        return new Response("MESSAGE", username + ", you can not afford to buy dishonor.\nYou need " + buyFailResult.MISSING_CURRENCY + " more guilt to buy 1 dishonor.");
+        return new Response(username + ", you can not afford to buy dishonor.\nYou need " + buyFailResult.MISSING_CURRENCY + " more guilt to buy 1 dishonor.");
     }
 
     private Response buySpell(String uuid, String username) {
         BuyResult buyResult = api.buySpell(uuid);
         if (buyResult instanceof BuySuccessResult) {
             BuySuccessResult buySuccessResult = (BuySuccessResult) buyResult;
-            return new Response("MESSAGE", username + ", you bought 1 spell for " + buySuccessResult.SPENT_AMOUNT + " pride/shame.");
+            return new Response(username + ", you bought 1 spell for " + buySuccessResult.SPENT_AMOUNT + " pride/shame.");
         }
-        return new Response("MESSAGE", username + ", you failed to buy a spell.");
+        return new Response(username + ", you failed to buy a spell.");
     }
 
     private Response stats(Command command, Guild guild) {
@@ -336,7 +435,7 @@ public class BotModel {
             uuid = command.getAuthor().getId();
         }
         String name = guild.getMemberById(uuid).getEffectiveName();
-        return new Response("MESSAGE", name +  "'s Stats:" +
+        return new Response(name +  "'s Stats:" +
                 "\n\tPride: " + api.getPride(uuid) +
                 "\n\tShame: " + api.getShame(uuid) +
                 "\n\tEgo: " + api.getEgo(uuid) +
@@ -347,7 +446,7 @@ public class BotModel {
     }
 
     private Response gameinfo(Command command) {
-        return new Response("MESSAGE", "PrideBot.Game Info:" +
+        return new Response("PrideBot.Game Info:" +
                 "\n\tPride and Shame:" +
                 "\n\t\tThe two atomic values of the game." +
                 "\n\t\tEffectively a currency." +
@@ -363,7 +462,7 @@ public class BotModel {
     }
 
     private Response botinfo(Command command) {
-        return new Response("MESSAGE", "PrideBot.Bot Info:" +
+        return new Response("PrideBot.Bot Info:" +
                 "\n\tUptime: " + getUptime() + " days" +
                 "\n\tVersion: v2.2" +
                 "\n\tDeveloper: Glyphical" +
@@ -376,7 +475,7 @@ public class BotModel {
     }
 
     private Response commands(Command command) {
-        return new Response("MESSAGE", "Commands:" +
+        return new Response("Commands:" +
                 "\n\t" + COMMANDS_MESSAGE_BLESS +
                 "\n\t" + COMMANDS_MESSAGE_CURSE +
                 "\n\t" + COMMANDS_MESSAGE_COLLECT +
@@ -393,7 +492,7 @@ public class BotModel {
     }
 
     private Response help(Command command) {
-        return new Response("MESSAGE", "Help:" +
+        return new Response("Help:" +
                 "\n\tAll commands use the p> prefix. This prefix is case-sensitive." +
                 "\n\tCommand names and arguments are not case-sensitive." +
                 "\n\tUse p>commands to get the list of commands." +
--- src/main/java/PrideBot/Bot/Response.java
+++ src/main/java/PrideBot/Bot/Response.java
@@ -2,30 +2,25 @@ package PrideBot.Bot;
 
 public class Response {
     public final String TARGET_UUID;
-    public final String TYPE;
     public final String[] MESSAGES;
 
-    public Response(String type, String message) {
+    public Response(String message) {
         this.TARGET_UUID = null;
-        this.TYPE = type;
         this.MESSAGES = new String[]{message};
     }
 
-    public Response(String targetUUID, String type, String message) {
+    public Response(String targetUUID, String message) {
         this.TARGET_UUID = targetUUID;
-        this.TYPE = type;
         this.MESSAGES = new String[]{message};
     }
 
-    public Response(String type, String[] messages) {
+    public Response(String[] messages) {
         this.TARGET_UUID = null;
-        this.TYPE = type;
         this.MESSAGES = messages;
     }
 
-    public Response(String targetUUID, String type, String[] messages) {
+    public Response(String targetUUID, String[] messages) {
         this.TARGET_UUID = targetUUID;
-        this.TYPE = type;
         this.MESSAGES = messages;
     }
 }
